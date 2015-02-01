3484f96e694262f6b158776ea7a491a52399ccfc
Clay Tobolka <tobolkac@gmail.com>
2015-02-01 09:45:02 -0600

    images in grid view are now in a card view, just for fun

---
+++ backend/src/main/java/com/example/tobolkac/googlecloudplatformexample/backend/ImageRecord.java
@@ -0,0 +1,14 @@
+package com.example.tobolkac.googlecloudplatformexample.backend;
+
+import com.googlecode.objectify.annotation.Entity;
+
+/**
+ * Created by clayt on 2/1/15.
+ */
+@Entity
+public class ImageRecord
+{
+    private String username;
+    private String password;
+    private String email;
+}
